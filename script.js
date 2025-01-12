05359607e53c3d72095a9bc7bcad9887f6bb88be
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-13 02:16:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let a=document.getElementById("fname").value;
+	let b=document.getElementById("lname").value;
+   let message=a+b;
+	alert(message);
 }
